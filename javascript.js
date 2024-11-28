0a6130d5ef17d8f3fd275cd00baf264b4dd4c685
alagozfurkan <alagozf30@gmail.com>
2024-11-28 13:31:20 +0300

    make grid size changeable by user input

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-containerdiv = document.querySelector(".container");
+    let containerdiv = document.querySelector(".container");
+    let allboxes = document.querySelector(".boxes");
+    let gridsize = document.querySelector(".gridsize");
+
     
     
     function createBox(num) {
@@ -7,14 +10,11 @@ containerdiv = document.querySelector(".container");
         let box = document.createElement("div");
         
         box.addEventListener("mouseenter",  (e) => {
-          e.target.style.background = "blue";
-          
-        });
+          e.target.style.background = "black"
 
-        box.addEventListener("mouseleave", (e) => {
-          e.target.style.background = ""
-        });
+          });
 
+      
         let boxheight = 600 / num;
         let boxwidht = 600 / num;
         box.style.height = `${boxheight}px`;
@@ -25,10 +25,25 @@ containerdiv = document.querySelector(".container");
 
       }
 
-      
         };
 
-
     
+    
+  
+    
+    createBox(5);
 
-    createBox(40);
+    gridsize.addEventListener("click", function() {
+
+        let desiredgridsize = prompt("number? ");
+        if (desiredgridsize > 100) {
+          alert("Gridsize cannot be bigger than 100!!!");
+        } else {
+          while (containerdiv.firstChild) 
+            containerdiv.removeChild(containerdiv.firstChild);
+          createBox(desiredgridsize);
+        }
+        
+        
+    
+    });
